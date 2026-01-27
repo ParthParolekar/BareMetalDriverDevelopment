d6227e57b47d90c11e2047e339e306baf98e45dd
Parth-BrandNav <parth@outboundgenie.com>
2026-01-27 16:18:31 +0530

    GPIO Pin and Port Input Outupt API

--- drivers/Src/stm32g070xx_gpio_driver.c
+++ drivers/Src/stm32g070xx_gpio_driver.c
@@ -121,23 +121,50 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx){
 
 //Data read and write
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber){
-
+	uint8_t value;
+	value = (uint8_t)pGPIOx->IDR >> PinNumber & (0x00000001);
+	return value;
 }
 
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx){
-
+	uint16_t value;
+	value = (uint16_t)pGPIOx->IDR;
+	return value;
 }
 
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value){
 
+	if(Value = GPIO_PIN_SET){
+
+		pGPIOx->ODR |= 0x1 << PinNumber;
+	}else if(Value = GPIO_PIN_RESET){
+
+		pGPIOx->ODR &= ~(0x1 << PinNumber);
+	}
+
 }
 
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value){
-
+	pGPIOx->ODR = Value;
 }
 
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber){
+	//XOR operator `^` is useful for toggling bits.
+	//Masking with 0 does not change the state
+	//Masking with 1 flips the current state
+	/*
 
+	____________________________________________
+	|	Bit A		Mask Bit B		Results A^B	|
+	|-------------------------------------------|
+	|	0			0				0			|
+	|	1			0				1			|
+	|	0			1				1			|
+	|	1			1				0			|
+	|___________________________________________|
+
+	*/
+	pGPIOx->ODR ^= 1 << PinNumber;
 }
 
 //IRQ configuration and ISR handling
